d6a3f2e826c719fb5400654795d545403c50a625
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2022-04-26 10:02:34 +0900

    fix index of file.

--- src/FilesUploadAndImageResize.php
+++ src/FilesUploadAndImageResize.php
@@ -184,9 +184,9 @@ class FilesUploadAndImageResize
 					if (in_array(strtolower($fileInfo), array_map('strtolower', $this->allowExtension)) || empty($this->allowExtension)) {
 						// Upload and compress only images
 						if (strtolower($fileInfo) == 'gif' || strtolower($fileInfo) == 'jpeg' || strtolower($fileInfo) == 'jpg' || strtolower($fileInfo) == 'png') {
-							if ($_FILES[$fileParamName]['tmp_name']['error'] > 0) {
+							if (isset($_FILES[$fileParamName]['error']) && ($_FILES[$fileParamName]['error']) > 0) {
 								$this->param['not_uploaded_files'][]	=	$fileName; //All not move files name into the destination folder [ Note: Check Folder Permission ]
-								$this->param['not_uploaded_files_error'][]	=	$this->phpFileUploadErrors[$_FILES[$fileParamName]['tmp_name']['error']]; //All not move files name into the destination folder with error [ Note: Check Folder Permission ]
+								$this->param['not_uploaded_files_error'][]	=	$this->phpFileUploadErrors[$_FILES[$fileParamName]['error']]; //All not move files name into the destination folder with error [ Note: Check Folder Permission ]
 							} else {
 								if ($this->compressImage($_FILES[$fileParamName]['tmp_name'][$this->n], $filePath, $minImgWidth, $waterMark, $quality, $newWidth)) {
 									if (isset($thumbWidth) && !empty($thumbWidth)) {
